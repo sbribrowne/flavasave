676660d828d95ce5288c744a6e1d4fb9ef8ddc63
Mark Kazanski <markkazanski@gmail.com>
2018-03-04 17:20:54 -0600

    refactored models

--- models/recipe.js
+++ models/recipe.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+
+const ingredientSchema = new Schema({
+  _id: {type: String, required: true},
+  text: String,
+  amount: Number,
+  unit: String
+});
+
+const instructionSchema = new Schema({
+  _id: {type: String, required: true},
+  text: String
+});
+
 const recipeSchema = new Schema({
+  userId: { type: String, required: true },
   title: { type: String, required: true },
   url: { type: String, required: true },
-  ingredients: [String],
-  instructions: [String],
+  ingredients: [ingredientSchema],
+  instructions: [instructionSchema],
   cooked: Boolean,
   imageUrl: String,
   tags: [String],
