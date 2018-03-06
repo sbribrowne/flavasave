aa1651712647e7d84564fcfbba54f4f146095c33
Alize Nguyen <alizenguyen@gmail.com>
2018-03-06 14:53:02 -0600

    navbar clean up and switched class to className

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,25 +1,22 @@
 import React from "react";
 
 const Nav = () => (
-  <nav class="navbar navbar-expand-lg navbar-light sticky-top bg-light">
-    <a class="navbar-brand" href="#">
+  <nav className="navbar navbar-expand-lg navbar-light sticky-top bg-light">
+    <a className="navbar-brand" href="#">
       <img src="../../images/logo_green.png" alt="FlavaSave" height="42" width="42" />
     </a>
-    <div
-      class="collapse navbar-collapse justify-content-end"
-      id="navbarNavAltMarkup"
-    >
-      <div class="navbar-nav">
-        <a class="nav-item nav-link" href="#">
+    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-item nav-link" href="#">
           About
         </a>
-        <a class="nav-item nav-link" href="#">
+        <a className="nav-item nav-link" href="#">
           How It Works
         </a>
-        <a class="nav-item nav-link" href="#">
+        <a className="nav-item nav-link" href="#">
           Sign In
         </a>
-        <a class="nav-item nav-link" href="#">
+        <a className="nav-item nav-link" href="#">
           Sign Up
         </a>
       </div>
