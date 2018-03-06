09f1abc5b76587fdb9e6ce0e4ea200eb27a58b30
Alize Nguyen <alizenguyen@gmail.com>
2018-03-06 15:47:03 -0600

    navbar css updates

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,7 +3,7 @@ import React from "react";
 const Nav = () => (
   <nav className="navbar navbar-expand-lg" id="homeNav">
     <a className="navbar-brand" href="#">
-      <img src={require('../../images/logo_green.png')} alt="FlavaSave" height="60" />
+      <img src={require('../../images/logo_green.png')} alt="FlavaSave" height="55" />
     </a>
     <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
       <div className="navbar-nav">
