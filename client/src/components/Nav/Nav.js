5435de9888f113b969a197d4536edd208c1fb98e
JQuinnie <jenn8866@gmail.com>
2018-03-05 20:44:03 -0600

    test

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,5 +1,20 @@
 import React from "react";
 
-const Nav = () => <div />;
+const Nav = () => (
+  <nav className="navbar navbar-inverse navbar-top">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button type="button" className="collapsed navbar-toggle">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar" /> <span className="icon-bar" />
+          <span className="icon-bar" />
+        </button>
+        <a href="/" className="navbar-brand">
+          FlavaSave!
+        </a>
+      </div>
+    </div>
+  </nav>
+);
 
 export default Nav;
