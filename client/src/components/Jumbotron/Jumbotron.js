8c20a7e843755e54387ec2d1e867fea59f59179d
Nicholas Sladden <nickoliasxii@gmail.com>
2018-03-05 19:38:47 -0600

    starting point

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Jumbotron = ({ children }) =>
+  <div style={{ height: 300, clear: 'both' }} className="jumbotron">
+    {"HELLO"}
+  </div>;
+
+export default Jumbotron;
