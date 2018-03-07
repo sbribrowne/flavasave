6c54e3a1bbca27641d8d9dbfe08cb2edcd64402c
Alize Nguyen <alizenguyen@gmail.com>
2018-03-07 15:13:23 -0600

    About component and css edits

--- client/src/components/Home/About.js
+++ client/src/components/Home/About.js
@@ -6,7 +6,7 @@ const About = () => (
       <div className="col-md-12 card">
         <img className="card-img-top" src={require('../../images/mitts.png')} alt="Store Image" height = "90" />
         <div className="card-body">
-          <h5 className="card-title">Store.</h5>
+          <h5 className="card-title">STORE.</h5>
           <p className="card-text">
             Store all the recipes you come across while browsing the internet in
             just one place.
@@ -18,7 +18,7 @@ const About = () => (
     <div className="col-md-12 card">
         <img className="card-img-top" src={require('../../images/spatulas.png')} alt="Organize Image" height = "90" />
         <div className="card-body">
-          <h5 className="card-title">Organize.</h5>
+          <h5 className="card-title">ORGANIZE.</h5>
           <p className="card-text">
             Search through and organize recipes that you've cooked or ones that
             need to be cooked.
@@ -30,10 +30,10 @@ const About = () => (
       <div className="col-md-12 card">
         <img className="card-img-top" src={require('../../images/chef_green.png')} alt="Edit Image" height = "90" />
         <div className="card-body">
-          <h5 className="card-title">Edit.</h5>
+          <h5 className="card-title">EDIT.</h5>
           <p className="card-text">
-            Edit ingredients and instructions to your liking and mark the
-            ingredients to create your shopping list.
+            Edit ingredients and instructions to your liking and check the
+            ingredients you already have.
           </p>
         </div>
       </div>
