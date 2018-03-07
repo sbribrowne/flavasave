1f62d9ac5980c00edb895076f5e7b4531ae08fbc
Alize Nguyen <alizenguyen@gmail.com>
2018-03-06 20:52:19 -0600

    login font edits

--- client/src/components/Home/Login.js
+++ client/src/components/Home/Login.js
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => (
   <div className="loginBox">
     <img className="card-img-top" src={require('../../images/logo_white.png')} alt="Store Image" height = "60" />
-    <h3>Your One-Stop Recipe Library</h3>
+    <h3 className="loginSubTitle">Your One-Stop Recipe Library</h3>
     <ol>
       <li>Create and Accout or Login</li>
       <li>
