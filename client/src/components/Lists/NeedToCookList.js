84818c27a7dfb6967773162ac8aa29daf86044ad
Nicholas Sladden <nickoliasxii@gmail.com>
2018-03-06 18:37:32 -0600

    NTC List

---
+++ client/src/components/Lists/NeedToCookList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NTCListItem from "./NTCListItem";
+
+const NeedToCookList = (props) => (
+  <ul className="list-group">
+      <NTCListItem/>
+  </ul>
+);
+
+export default NeedToCookList;
