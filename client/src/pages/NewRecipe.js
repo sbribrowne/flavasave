d179841c7588fe643b8bfe839fe7749ced67b4f2
sbribrowne <sbribrowne@gmail.com>
2018-03-06 20:19:10 -0600

    swap newrecipe and userpage

--- client/src/pages/NewRecipe.js
+++ client/src/pages/NewRecipe.js
@@ -1,11 +1,46 @@
 import React from "react";
 import Nav from "../components/Nav/Nav.js";
+import Input from "../components/Forms/Input.js";
+import FormBtn from "../components/Forms/FormBtn.js";
+import DropDwn from "../components/Forms/DropDwn.js";
 
 const NewRecipe = () => {
   return (
     <div>
       <Nav />
-      <h1>NewRecipe Page Component goes here</h1>
+      <div>
+        <h3>Recipe Name</h3>
+        <form class='row'>
+          <Input name="recipe-name" placeholder="Recipe Name" />
+          <FormBtn>Submit</FormBtn>
+        </form>
+
+        <h3>Serving Size</h3>
+        <form class='row'>
+          <Input name="serving-size" placeholder="Serving Size" />
+          <FormBtn>Submit</FormBtn>
+        </form>
+
+        <h3>Ingredients</h3>
+        <form class='row'>
+          <Input name="amount" placeholder="Amount" />
+          <DropDwn />
+          <Input name="ingredient" placeholder="Ingredient" />
+          <FormBtn>Submit</FormBtn>
+        </form>
+
+        <h3>Instructions</h3>
+        <form class='row'>
+          <Input name="instructions" placeholder="Instructions" />
+          <FormBtn>Submit</FormBtn>
+        </form>
+
+        <h3>Tags</h3>
+        <form class='row'>
+          <Input name="tags" placeholder="Tags" />
+          <FormBtn>Submit</FormBtn>
+        </form>
+      </div>
     </div>
   );
 };
--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -1,46 +1,10 @@
 import React from "react";
 import Nav from "../components/Nav/Nav.js";
-import Input from "../components/Forms/Input.js";
-import FormBtn from "../components/Forms/FormBtn.js";
-import DropDwn from "../components/Forms/DropDwn.js";
 
 const UserPage = () => {
   return (
     <div>
       <Nav />
-      <div>
-        <h3>Recipe Name</h3>
-        <form class='row'>
-          <Input name="recipe-name" placeholder="Recipe Name" />
-          <FormBtn>Submit</FormBtn>
-        </form>
-
-        <h3>Serving Size</h3>
-        <form class='row'>
-          <Input name="serving-size" placeholder="Serving Size" />
-          <FormBtn>Submit</FormBtn>
-        </form>
-
-        <h3>Ingredients</h3>
-        <form class='row'>
-          <Input name="amount" placeholder="Amount" />
-          <DropDwn />
-          <Input name="ingredient" placeholder="Ingredient" />
-          <FormBtn>Submit</FormBtn>
-        </form>
-
-        <h3>Instructions</h3>
-        <form class='row'>
-          <Input name="instructions" placeholder="Instructions" />
-          <FormBtn>Submit</FormBtn>
-        </form>
-
-        <h3>Tags</h3>
-        <form class='row'>
-          <Input name="tags" placeholder="Tags" />
-          <FormBtn>Submit</FormBtn>
-        </form>
-      </div>
     </div>
   );
 };
