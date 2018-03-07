5569a5c9fecf84ebd2b65f0080118accd9b0a476
JQuinnie <jenn8866@gmail.com>
2018-03-06 19:43:23 -0600

    Add Howitworks component to Home page

---
+++ client/src/components/Home/Howitworks.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Howitworks = () => (
+  <div className="container bg-warning">
+    <h1>HOW IT WORKS</h1>
+    <ol>
+      <li>Create and Accout or Login</li>
+      <li>
+        All you need is the recipe URL. Input the URL into the text area on your
+        homepage. The app will do its magic, and your recipe will be added to
+        your account.
+      </li>
+      <li>Explore our features and start cooking!</li>
+    </ol>
+  </div>
+);
+
+export default Howitworks;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Nav from "../components/Nav/Nav.js";
 import About from "../components/Home/About.js";
+import Howitworks from "../components/Home/Howitworks.js";
 import "../stylesheets/css/main.css";
 
 const Home = () => {
@@ -8,8 +9,10 @@ const Home = () => {
     <div>
       <Nav />
       <br />
+      <br />
       <About />
       <br />
+      <Howitworks />
     </div>
   );
 };
