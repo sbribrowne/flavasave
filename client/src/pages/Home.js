9ba43b4e621e6c4a6a2dbcce1be35a560ce7578e
Alize Nguyen <alizenguyen@gmail.com>
2018-03-06 19:32:34 -0600

    nav css fixed and footer layout fixed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "../components/Nav/Nav.js";
+import Footer from "../components/Footer/Footer.js";
 import "../stylesheets/css/main.css"
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div class="homebody">
       <Nav />
       <h1>SignUp Page Component goes here</h1>
+      <Footer/>
     </div>
   );
 };
