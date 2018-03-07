21c290c333d347e4482c513b3de63a51cf5e9733
JQuinnie <jenn8866@gmail.com>
2018-03-06 19:58:55 -0600

    Add Login component to Home page.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Nav from "../components/Nav/Nav.js";
+import Login from "../components/Home/Login.js";
 import About from "../components/Home/About.js";
 import Howitworks from "../components/Home/Howitworks.js";
 import "../stylesheets/css/main.css";
@@ -8,6 +9,13 @@ const Home = () => {
   return (
     <div>
       <Nav />
+      <br />
+      <div className="row">
+        <div className="col-4" />
+        <div className="col-8">
+          <Login />
+        </div>
+      </div>
       <br />
       <br />
       <About />
