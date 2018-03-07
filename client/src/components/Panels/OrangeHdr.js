d99ddd7ac3063f933817417877d0eef972a53b3b
sbribrowne <sbribrowne@gmail.com>
2018-03-06 21:20:00 -0600

    fix orange header

--- client/src/components/Panels/OrangeHdr.js
+++ client/src/components/Panels/OrangeHdr.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Panel = (props) => (
+const OrangeHdr = (props) => (
   <div className="panel text-center col-md-6">
     <img src={props.image} />
     <h1>{props.name}</h1>
   </div>
 );
 
-export default Panel;
+export default OrangeHdr;
