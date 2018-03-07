1f86e07d9e3358ece240271bce53cd4ed99ea7a4
Alize Nguyen <alizenguyen@gmail.com>
2018-03-07 15:34:51 -0600

    final about css edits. columns made equal.

--- client/src/components/Home/About.js
+++ client/src/components/Home/About.js
@@ -1,32 +1,32 @@
 import React from "react";
 
 const About = () => (
-  <div className="row card-deck">
-    <div className="col-md-4">
+  <div className="row card-deck row-eq-height">
+    <div className="col-md-4 big-card">
       <div className="col-md-12 card">
         <img className="card-img-top" src={require('../../images/mitts.png')} alt="Store Image" height = "90" />
         <div className="card-body">
           <h5 className="card-title">STORE.</h5>
           <p className="card-text">
             Store all the recipes you come across while browsing the internet in
-            just one place.
+            just one place with just the URL.
           </p>
         </div>
       </div>
     </div>
-    <div className="col-md-4">
+    <div className="col-md-4 big-card">
     <div className="col-md-12 card">
         <img className="card-img-top" src={require('../../images/spatulas.png')} alt="Organize Image" height = "90" />
         <div className="card-body">
           <h5 className="card-title">ORGANIZE.</h5>
           <p className="card-text">
-            Search through and organize recipes that you've cooked or ones that
+            Search through and organize recipes that you have already cooked or ones that
             need to be cooked.
           </p>
         </div>
       </div>
     </div>
-    <div className="col-md-4">
+    <div className="col-md-4 big-card">
       <div className="col-md-12 card">
         <img className="card-img-top" src={require('../../images/chef_green.png')} alt="Edit Image" height = "90" />
         <div className="card-body">
