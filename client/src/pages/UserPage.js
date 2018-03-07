0b363d0a63ff122a46e1864598dedf74faa5ccfa
sbribrowne <sbribrowne@gmail.com>
2018-03-06 20:26:50 -0600

    add form for userpage

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -5,6 +5,23 @@ const UserPage = () => {
   return (
     <div>
       <Nav />
+      <h3>ADD RECIPE BY URL</h3>
+      <form class='row'>
+        <Input name="add-recipe" />
+        <FormBtn>Submit</FormBtn>
+      </form>
+
+      <h3>SEARCH RECIPES</h3>
+      <form class='row'>
+        <Input name="search-recipe" />
+        <FormBtn>Submit</FormBtn>
+      </form>
+
+      <h3>SEARCH BY TAGS</h3>
+      <form class='row'>
+        <Input name="search-tags" />
+        <FormBtn>Submit</FormBtn>
+      </form>
     </div>
   );
 };
