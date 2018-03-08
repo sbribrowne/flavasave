7b4ca64312bdc84d9e107cae534df9a2dcafa70e
JQuinnie <jenn8866@gmail.com>
2018-03-07 22:35:37 -0600

    Fix error class to className

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,25 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => (
-
-  <nav class="navbar" id="homeNav">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" id="navbarlogo" href="#"><img src={require('../../images/logo_green.png')} alt="FlavaSave" height="60" /></a>
+  <nav className="navbar" id="homeNav">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <a className="navbar-brand" id="navbarlogo" href="#">
+          <img
+            src={require("../../images/logo_green.png")}
+            alt="FlavaSave"
+            height="60"
+          />
+        </a>
+      </div>
+      <div
+        className="collapse navbar-collapse justify-content-end"
+        id="navbarNavAltMarkup"
+      >
+        <div className="nav navbar-nav navbar-right">
+          <Link className="nav-item nav-link" to="/">
+            ABOUT
+          </Link>
+          <Link className="nav-item nav-link" to="/">
+            HOW IT WORKS
+          </Link>
+          <Link className="nav-item nav-link" to="/signup">
+            SIGN UP
+          </Link>
         </div>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-          <div className="nav navbar-nav navbar-right">
-            <Link className="nav-item nav-link" to="/">
-              ABOUT
-            </Link>
-            <Link className="nav-item nav-link" to="/">
-              HOW IT WORKS
-            </Link>
-            <Link className="nav-item nav-link" to="/signup">
-              SIGN UP
-            </Link>
-          </div>
-        </div>
+      </div>
     </div>
   </nav>
 );
