0c81f72be8b00a743fe838d921165ba6db7d568d
Mark Kazanski <markkazanski@gmail.com>
2018-03-07 21:06:47 -0600

    setup all routes, including login

--- controllers/ingredient-api-routes.js
+++ controllers/ingredient-api-routes.js
@@ -1,36 +1,45 @@
 const db = require("../models");
 
 module.exports = function (app) {
-
-    app.get("/api/ingredients/:id", function (req, res) {
-        //get ingredients
+    //get ALL ingredients for Recipe
+    app.get("/api/ingredients/:recipeId", function (req, res) {
         db.Ingredient.findAll({
             where: {
-                RecipeId: req.params.id
+                RecipeId: req.params.recipeId
             }
         }).then(function (data) {
             res.json(data);
         });
     });
 
+    //add new ingre to recipe - include RecipeId in object
     app.post("/api/ingredients", function (req, res) {
-        //add ingredients
-        console.log(req.body);
-        db.Ingredient.create(req.body).then(function (Ingredient) {
-            res.json(Ingredient); //Says RecipeId cannot be null...even when there's a number.
+        db.Ingredient.create(req.body.ingredientObj)
+        .then(function (dbIngredient) {
+            res.json(dbIngredient);
         });
     });
 
-    app.put("/api/ingredients/edit/:id", function (req, res) {
-        db.Ingredient.update({
-            ingredient_info: req.body.ingredient_info,
-        }, {
+    //Edit ingre - include RecipeId in object
+    app.put("/api/ingredients/:ingredientId", function (req, res) {
+        db.Ingredient.update(
+            req.body.ingredientObj, {
                 where: {
-                    id: req.params.id
+                    id: req.params.ingredientId
                 }
-            }).then(function (dbPost) {
-            console.log("Edited went through");
-            res.json(dbPost);
-        });
+            }).then(function (dbIngredient) {
+                res.json(dbIngredient); //edit complete
+            });
+    });
+
+    //DELETE
+    app.delete("/api/ingredients/:ingredientId", function (req, res) {
+        db.Ingredient.destroy({ 
+            where: {
+                id: req.params.ingredientId
+            }
+        }).then(function (dbIngredient) {
+                res.json(dbIngredient); //deleted
+            });
     });
 };
