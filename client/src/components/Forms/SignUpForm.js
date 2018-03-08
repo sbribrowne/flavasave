b111afc83f035c37071cd068bc7ca04a563763cb
JQuinnie <jenn8866@gmail.com>
2018-03-07 22:07:50 -0600

    Add onChange and onSubmit functions to the sign up form

--- client/src/components/Forms/SignUpForm.js
+++ client/src/components/Forms/SignUpForm.js
@@ -1,41 +1,84 @@
 import React from "react";
 
-const SignUpForm = props => (
-  <div className="sign-up-form">
-    <form className="text-center">
-      <div class="form-group">
-        <label htmlFor="firstname">First Name</label>
-        <input type="text" class="form-control" name="username" />
+class SignUpForm extends React.Component {
+  state = {
+    firstname: "",
+    lastname: "",
+    username: "",
+    email: "",
+    password: ""
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="sign-up-form">
+        <form className="text-center signUpForm" onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label htmlFor="firstname">First Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="firstname"
+              value={this.state.firstname}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastname">Last Name</label>
+            <input
+              type="text"
+              className="form-control"
+              name="lastname"
+              value={this.state.lastname}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              value={this.state.username}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              value={this.state.email}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              value={this.state.password}
+              onChange={this.onChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary btn-lg">
+            Sign Up
+          </button>
+        </form>
       </div>
-      <div class="form-group">
-        <label htmlFor="lastname">Last Name</label>
-        <input type="text" class="form-control" name="username" />
-      </div>
-      <div class="form-group">
-        <label htmlFor="username">Username</label>
-        <input type="text" class="form-control" name="username" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          className="form-control"
-          onChange={props.handleInputChange}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          className="form-control"
-          onChange={props.handleInputChange}
-        />
-      </div>
-      <button onClick={props.handleOnSubmit} className="btn btn-primary btn-lg">
-        Sign Up
-      </button>
-    </form>
-  </div>
-);
+    );
+  }
+}
 
 export default SignUpForm;
