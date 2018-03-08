282029487a0b68f5baeb43dfb41b44b64736645a
Alize Nguyen <alizenguyen@gmail.com>
2018-03-07 22:29:29 -0600

    home page content added and edited

--- client/src/components/Home/Readmore.js
+++ client/src/components/Home/Readmore.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const ReadMore = () => (
   <div className="readmore-block">
-    <h3 className="readmore-text">READ MORE</h3>
-    <img className="arrow" src={require('../../images/arrow.png')} alt="Arrow Image" />
+    <div className="readmore-description">
+      <p>Save recipes from the web.</p>
+      <p id="readmore-organize">Organize as you go.</p>
+    </div>
+    <div className="readmore-subBlock">
+      <h3 className="readmore-text">READ MORE</h3>
+      <img className="arrow" src={require('../../images/arrow.png')} alt="Arrow Image" />
+    </div>
   </div>
 );
 
