54cb834adda2964c34e7d214302848787e241fd7
JQuinnie <jenn8866@gmail.com>
2018-03-07 22:34:22 -0600

    Update signup to stateful component

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -3,33 +3,35 @@ import Nav from "../components/Nav/Nav.js";
 import SignUpForm from "../components/Forms/SignUpForm";
 import FooterLogged from "../components/Footer/FooterLogged.js";
 
-const SignUp = () => {
-  return (
-    <div>
-      <Nav />
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-4 panel panel-heading panel-success">
-            <img
-              className="card-img-top"
-              src={require("../images/chef_green.png")}
-              height="180"
-            />
-            <br />
-            <h1>Start Cooking</h1>
-            <br />
-            <h4>
-              <i>with your one-stop recipe library!</i>
-            </h4>
-          </div>
-          <div className="col-sm-8">
-            <SignUpForm />
+class SignUp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4 panel panel-heading panel-success">
+              <img
+                className="card-img-top"
+                src={require("../images/chef_green.png")}
+                height="180"
+              />
+              <br />
+              <h1>Start Cooking</h1>
+              <br />
+              <h4>
+                <i>with your one-stop recipe library!</i>
+              </h4>
+            </div>
+            <div className="col-sm-8">
+              <SignUpForm />
+            </div>
           </div>
         </div>
+        <FooterLogged />
       </div>
-      <FooterLogged />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default SignUp;
