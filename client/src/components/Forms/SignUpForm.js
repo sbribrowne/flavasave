3de23b1392da2e74b0e6c4c7e432aefde0438c3a
JQuinnie <jenn8866@gmail.com>
2018-03-10 01:25:18 -0600

    Refractor signup text field to separate component, validator also refrac

--- client/src/components/Forms/SignUpForm.js
+++ client/src/components/Forms/SignUpForm.js
@@ -1,7 +1,8 @@
 import React from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
-import classnames from "classnames";
+import validateInput from "../../actions/validations/signup";
+import TextFieldGroup from "./TextFieldGroup";
 
 class SignUpForm extends React.Component {
   state = {
@@ -18,17 +19,28 @@ class SignUpForm extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  isValid() {
+    const { errors, isValid } = validateInput(this.state);
+    // if not valid, set and populate state with errors
+    if (!isValid) {
+      this.setState({ errors });
+    }
+    return isValid;
+  }
+
   onSubmit = event => {
-    // set state for errors to be passed to the object > clear errors > then repopulate
-    this.setState({ errors: {}, isLoading: true });
     event.preventDefault();
-    // console.log(this.state);
-    this.props
-      .userSignUpRequest(this.state)
-      .then(() => {})
-      .catch(error => {
-        this.setState({ errors: error.response.data, isLoading: false });
-      });
+
+    if (this.isValid()) {
+      // set state for errors to be passed to the object > clear errors > then repopulate
+      this.setState({ errors: {}, isLoading: true });
+      this.props
+        .userSignUpRequest(this.state)
+        .then(() => {})
+        .catch(error => {
+          this.setState({ errors: error.response.data, isLoading: false });
+        });
+    }
   };
 
   render() {
@@ -36,99 +48,41 @@ class SignUpForm extends React.Component {
     return (
       <div className="sign-up-form">
         <form className="signUpForm" onSubmit={this.onSubmit}>
-          <div
-            className={classnames("form-group", {
-              "has-error": errors.firstname
-            })}
-          >
-            <label htmlFor="firstname" className="signup-label">
-              FIRST NAME
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              name="firstname"
-              value={this.state.firstname}
-              onChange={this.onChange}
-            />
-            {errors.firstname && (
-              <span className="help-block">{errors.firstname}</span>
-            )}
-          </div>
-          <div
-            className={classnames("form-group", {
-              "has-error": errors.lastname
-            })}
-          >
-            <label htmlFor="lastname" className="signup-label">
-              LAST NAME
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              name="lastname"
-              value={this.state.lastname}
-              onChange={this.onChange}
-            />
-            {errors.lastname && (
-              <span className="help-block">{errors.lastname}</span>
-            )}
-          </div>
-          <div
-            className={classnames("form-group", {
-              "has-error": errors.username
-            })}
-          >
-            <label htmlFor="username" className="signup-label">
-              USERNAME
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              name="username"
-              value={this.state.username}
-              onChange={this.onChange}
-            />
-            {errors.username && (
-              <span className="help-block">{errors.username}</span>
-            )}
-          </div>
-          <div
-            className={classnames("form-group", {
-              "has-error": errors.email
-            })}
-          >
-            <label htmlFor="email" className="signup-label">
-              E-MAIL
-            </label>
-            <input
-              type="email"
-              name="email"
-              className="form-control"
-              value={this.state.email}
-              onChange={this.onChange}
-            />
-            {errors.email && <span className="help-block">{errors.email}</span>}
-          </div>
-          <div
-            className={classnames("form-group", {
-              "has-error": errors.password
-            })}
-          >
-            <label htmlFor="password" className="signup-label">
-              PASSWORD
-            </label>
-            <input
-              type="password"
-              name="password"
-              className="form-control"
-              value={this.state.password}
-              onChange={this.onChange}
-            />
-            {errors.password && (
-              <span className="help-block">{errors.password}</span>
-            )}
-          </div>
+          <TextFieldGroup
+            error={errors.firstname}
+            label="First Name"
+            onChange={this.onChange}
+            value={this.state.firstname}
+            field="firstname"
+          />
+          <TextFieldGroup
+            error={errors.lastname}
+            label="Last Name"
+            onChange={this.onChange}
+            value={this.state.lastname}
+            field="lastname"
+          />
+          <TextFieldGroup
+            error={errors.username}
+            label="Username"
+            onChange={this.onChange}
+            value={this.state.username}
+            field="username"
+          />
+          <TextFieldGroup
+            error={errors.email}
+            label="Email"
+            onChange={this.onChange}
+            value={this.state.email}
+            field="email"
+          />
+          <TextFieldGroup
+            error={errors.password}
+            label="Password"
+            onChange={this.onChange}
+            value={this.state.password}
+            field="password"
+          />
           <button
             type="submit"
             className="btn btn-primary btn-lg signup-button"
