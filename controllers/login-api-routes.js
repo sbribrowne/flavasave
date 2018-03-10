695b7fc924f5c9514a83b9bbc2260e66e4a83e3e
JQuinnie <jenn8866@gmail.com>
2018-03-10 01:25:43 -0600

    Refractor validator function to separate file

--- controllers/login-api-routes.js
+++ controllers/login-api-routes.js
@@ -1,38 +1,9 @@
 // Requiring our models and passport as we've configured it
 var db = require("../models");
 var passport = require("../config/passport");
-var Validator = require("validator");
-var isEmpty = require("lodash.isempty");
+var validateInput = require("../scripts/validations/signup");
 
 module.exports = function(app) {
-  // validateInput function for signup
-  function validateInput(data) {
-    let errors = {};
-
-    if (Validator.isEmpty(data.firstname)) {
-      errors.firstname = "This field is required";
-    }
-    if (Validator.isEmpty(data.lastname)) {
-      errors.lastname = "This field is required";
-    }
-    if (Validator.isEmpty(data.username)) {
-      errors.username = "This field is required";
-    }
-    if (Validator.isEmpty(data.email)) {
-      errors.email = "This field is required";
-    }
-    if (!Validator.isEmail(data.email)) {
-      errors.email = "Email is invalid";
-    }
-    if (Validator.isEmpty(data.password)) {
-      errors.password = "This field is required";
-    }
-    return {
-      errors,
-      isValid: isEmpty(errors)
-    };
-  }
-
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
