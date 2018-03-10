d3845efb32de9bc2ace27b013f962c166357870f
JQuinnie <jenn8866@gmail.com>
2018-03-09 23:47:56 -0600

    Change post route for user signup

--- client/src/actions/signUpActions.js
+++ client/src/actions/signUpActions.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export function userSignUpRequest(userData) {
   return dispatch => {
-    return axios.post("api/user", userData);
+    return axios.post("api/userSignUp", userData);
   };
 }
