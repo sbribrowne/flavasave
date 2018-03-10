4a1c02041f721355904916d1c820db45f00ee460
JQuinnie <jenn8866@gmail.com>
2018-03-10 17:36:18 -0600

    Fix class to className

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,28 +2,37 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => (
-
-  <nav class="navbar" id="homeNav">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <Link to="/">
-            <a className="navbar-brand" id="navbarlogo" href="#"><img id="navbarlogo" src={require('../../images/logo_green.png')} alt="FlavaSave" height="60" /></a>
-            </Link>
-        </div>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-          <div className="nav navbar-nav navbar-right">
-            <Link className="nav-item nav-link" to="/">
-              ABOUT
-            </Link>
-            <Link className="nav-item nav-link" to="/">
-              HOW IT WORKS
-            </Link>
-            <Link className="nav-item nav-link" to="/signup">
-              SIGN UP
-            </Link>
-          </div>
+  <nav className="navbar" id="homeNav">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <Link to="/">
+          <a className="navbar-brand" id="navbarlogo" href="#">
+            <img
+              id="navbarlogo"
+              src={require("../../images/logo_green.png")}
+              alt="FlavaSave"
+              height="60"
+            />
+          </a>
+        </Link>
+      </div>
+      <div
+        className="collapse navbar-collapse justify-content-end"
+        id="navbarNavAltMarkup"
+      >
+        <div className="nav navbar-nav navbar-right">
+          <Link className="nav-item nav-link" to="/">
+            ABOUT
+          </Link>
+          <Link className="nav-item nav-link" to="/">
+            HOW IT WORKS
+          </Link>
+          <Link className="nav-item nav-link" to="/signup">
+            SIGN UP
+          </Link>
         </div>
       </div>
+    </div>
   </nav>
 );
 
