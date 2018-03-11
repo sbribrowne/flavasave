e3dadeeaa54901f55014565c2abe587428d14788
JQuinnie <jenn8866@gmail.com>
2018-03-11 17:55:58 -0500

    Add Redux dev tools to project

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./rootReducer";
+import setAuthToken from "./utils/setAuthToken";
 
-const store = createStore((state = {}) => state, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+setAuthToken(localStorage.jwtToken);
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
