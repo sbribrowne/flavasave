8e29648c26ae2701d3676d89e258ad7fdc2aac6b
JQuinnie <jenn8866@gmail.com>
2018-03-11 17:56:50 -0500

    Use jwtoken on client and set current user in Redux store for user login

--- client/src/actions/loginActions.js
+++ client/src/actions/loginActions.js
@@ -1,7 +1,22 @@
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt from "jsonwebtoken";
+import { SET_CURRENT_USER } from "./types";
+
+export function setCurrentUser(user) {
+  return {
+    type: SET_CURRENT_USER,
+    user
+  };
+}
 
 export function loginAction(userData) {
   return dispatch => {
-    return axios.post("api/login", userData);
+    return axios.post("api/login", userData).then(res => {
+      const token = res.data.token; // this is user object
+      localStorage.setItem("jwtToken", token);
+      setAuthToken(token);
+      dispatch(setCurrentUser(jwt.decode(token)));
+    });
   };
 }
