4cb0fa317e1fdbaef6c1486111ecf57caa16dc9a
JQuinnie <jenn8866@gmail.com>
2018-03-12 16:40:55 -0500

    Fix className

--- client/src/components/Lists/CompleteListItem.js
+++ client/src/components/Lists/CompleteListItem.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Buttons from "../Buttons/Button";
 
-const CompleteListItem = (props) => (
+const CompleteListItem = props => (
   <div>
-    <div class="col-md-4">{"RECIPE GOES HERE"}</div>
-    <div class="col-md-4">{"SOURCE GOES HERE"}</div>
-    <div class="col-md-4"><Buttons /></div>
+    <div className="col-md-4">{"RECIPE GOES HERE"}</div>
+    <div className="col-md-4">{"SOURCE GOES HERE"}</div>
+    <div className="col-md-4">
+      <Buttons />
+    </div>
   </div>
 );
 
