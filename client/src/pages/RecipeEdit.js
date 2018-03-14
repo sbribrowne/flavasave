a05b19a20c52569e62a9984c68f0707c624fb531
Alize Nguyen <alizenguyen@gmail.com>
2018-03-14 17:32:55 -0500

    Default class name change

--- client/src/pages/RecipeEdit.js
+++ client/src/pages/RecipeEdit.js
@@ -5,7 +5,7 @@ import DropDwn from "../components/Forms/DropDwn.js";
 import NavLogged from "../components/Nav/NavLogged.js";
 import FooterLogged from "../components/Footer/FooterLogged.js";
 
-class AddNewRecipe extends Component {
+class RecipeEdit extends Component {
   //Need method for handling input submit for
 
   //We should add an input for saving images to be associated to the recipe created
@@ -81,4 +81,4 @@ class AddNewRecipe extends Component {
   }
 }
 
-export default AddNewRecipe;
+export default RecipeEdit;
