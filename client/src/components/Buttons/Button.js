667b7001937f22bc5af6fe8bcba7fe9c9610aa7b
Alize Nguyen <alizenguyen@gmail.com>
2018-03-14 15:20:37 -0500

    userpage styled (no media queries yet)

--- client/src/components/Buttons/Button.js
+++ client/src/components/Buttons/Button.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Buttons = () => (
   <span className="userpage-buttons">
-    <button className="btn btn-success btn-sm">Edit</button>
-    <button className="btn btn-danger btn-sm">Delete</button>
+    <button className="btn btn-success btn-sm up-edit-button">Edit</button>
+    <button className="btn btn-danger btn-sm up-delete-button">Delete</button>
   </span>
 );
 
