ed1e12125bd478a91ea95c3ee15e8404331881d3
Alize Nguyen <alizenguyen@gmail.com>
2018-03-14 15:36:28 -0500

    testing

--- client/src/pages/UserPage.js
+++ client/src/pages/UserPage.js
@@ -61,7 +61,7 @@ class UserPage extends Component {
     return (
       <div>
         <Nav />
-        <h1 className="greeting-text">WELCOME, PLACEHOLDER@PLACEHOLDER.COM.</h1>
+        <h1 className="greeting-text">WELCOME, TESTING@PLACEHOLDER.COM.</h1>
         <div className="container-fluid userpage-container">
           <h3 className="search-title">ADD RECIPE BY URL</h3>
           <form className="row">
