1792e3362abd17dc9bb1e5fde19949a882c89361
Alize Nguyen <alizenguyen@gmail.com>
2018-03-15 00:57:20 -0500

    button classes added

--- client/src/components/Buttons/EditBtn.js
+++ client/src/components/Buttons/EditBtn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const EditBtn = props => (
   <span>
-    <button className="btn btn-sm" {...props}>Edit</button>
+    <button className="btn btn-sm up-edit-button" {...props}>Edit</button>
   </span>
 );
 
