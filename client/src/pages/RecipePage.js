8966c227aacfcd7f81f0c04328f4d21f6beb4513
Mark Kazanski <markkazanski@gmail.com>
2018-03-14 21:05:36 -0500

    Instructions list working

--- client/src/pages/RecipePage.js
+++ client/src/pages/RecipePage.js
@@ -3,6 +3,8 @@ import NavLogged from "../components/Nav/NavLogged.js";
 import Panel from "../components/Panels/Panel.js";
 import IngredientList from "../components/Lists/IngredientList";
 import IngredientListItem from "../components/Lists/IngredientListItem";
+import InstructionList from "../components/Lists/InstructionList";
+import InstructionListItem from "../components/Lists/InstructionListItem";
 import FooterLogged from "../components/Footer/FooterLogged.js";
 import API from "../utils/API";
 import "../stylesheets/css/main.css";
@@ -10,10 +12,9 @@ import DeleteBtn from "../components/Buttons/DeleteBtn";
 
 class Recipes extends Component {
   state = {
-    recipe: {
-      Ingredients: ["asdfafd"]
-    },
-    ingredients: ["asdf"]
+    recipe: {},
+    ingredients: [],
+    instructions: []
   }
 
   componentDidMount() {
@@ -25,11 +26,9 @@ class Recipes extends Component {
     .then(
       res => {
         this.setState({ 
-          recipe: res.data
-        });
-
-        this.setState({ 
-          ingredients: res.data.Ingredients
+          recipe: res.data,
+          ingredients: res.data.Ingredients, 
+          instructions: res.data.Instructions 
         });
       }
     );
@@ -62,9 +61,7 @@ class Recipes extends Component {
           {this.state.ingredients.length ? ( //Check for Ingredients
             <IngredientList>
               {this.state.ingredients.map(ingredient => (
-                <IngredientListItem key={ingredient.id} data={ingredient}>
-                  <p>{ingredient.ingredient_info}</p>
-                </IngredientListItem>
+                <IngredientListItem key={ingredient.id} data={ingredient} />
               ))}
             </IngredientList>
             ) : (
@@ -78,11 +75,15 @@ class Recipes extends Component {
         {/* STAND IN DIRECTIONS SECTION */}
         <Panel>
           <h4>DIRECTIONS</h4>
-          <p>The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.
-            The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.
-            The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.
-            The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.
-        </p>
+          {this.state.instructions.length ? ( //Check for Instructions
+            <InstructionList>
+              {this.state.instructions.map(instruction => (
+                <InstructionListItem key={instruction.id} data={instruction} />
+              ))}
+            </InstructionList>
+            ) : (
+              <h3>No Results to Display</h3>
+            )} 
         </Panel>
         {/* STAND IN BUTTONS */}
         <Panel>
