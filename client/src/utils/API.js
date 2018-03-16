9b5735520d51a76783c5d659a33ea861f1beac0e
JQuinnie <jenn8866@gmail.com>
2018-03-15 21:52:00 -0500

    Add method for getting user data

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,24 +2,27 @@ import axios from "axios";
 
 export default {
   // Gets all recipes
-  getRecipes: function () {
+  getRecipes: function() {
     return axios.get("/api/recipes");
   },
   // Gets the recipe with the given id
-  getRecipe: function (id) {
+  getRecipe: function(id) {
     return axios.get(`/api/recipes/${id}`);
   },
   // Deletes the recipe with the given id
-  deleteRecipe: function (id) {
+  deleteRecipe: function(id) {
     return axios.delete(`/api/recipes/${id}`);
   },
   // Saves a recipe to the database
-  saveRecipe: function (newURL) {
+  saveRecipe: function(newURL) {
     console.log(newURL);
-    return axios.post("/api/recipes", newURL)
-      .then(function (data) {
-        console.log(data);
-        return data;
-      })
+    return axios.post("/api/recipes", newURL).then(function(data) {
+      console.log(data);
+      return data;
+    });
+  },
+  // Gets the user data with give id
+  getUserData: function() {
+    return axios.get("/api/user_data");
   }
 };
