2f30b27cedb1fb63bd77fbb7bf169850685ab497
Alize Nguyen <alizenguyen@gmail.com>
2018-03-17 10:54:31 -0500

    added new instruction and new ingredient input

--- client/src/pages/RecipeEdit.js
+++ client/src/pages/RecipeEdit.js
@@ -174,6 +174,17 @@ class RecipeEdit extends Component {
               ))}
           </div>
 
+          <h3 className="ERTitle">ADD NEW INGREDIENT</h3>
+          <div className="row">
+            <div className="recipe-page-col col-sm-11">
+              <Input className="ERInput" name="recipe-name" />
+            </div>
+            <div className="recipe-page-col col-sm-1">
+              <button className="btn ERSubmit" type="button">save</button>
+            </div>
+          </div>
+
+
           <h3 className="ERTitle">INSTRUCTIONS</h3>
           <div>
               {this.state.instructions.map(instruction => (
@@ -194,6 +205,16 @@ class RecipeEdit extends Component {
                   </div>
                 </div>
               ))}
+          </div>
+
+          <h3 className="ERTitle">ADD NEW INSTRUCTION</h3>
+          <div className="row">
+            <div className="recipe-page-col col-sm-11">
+              <Input className="ERInput" name="recipe-name" />
+            </div>
+            <div className="recipe-page-col col-sm-1">
+              <button className="btn ERSubmit" type="button">save</button>
+            </div>
           </div>
 
           <h3 className="ERTitle">TAGS</h3>
