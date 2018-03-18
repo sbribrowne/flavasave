dd157bf7ed66e4fcf833b1c4e21eff236f7e7a2e
JQuinnie <jenn8866@gmail.com>
2018-03-18 15:40:07 -0500

    Fix typo

--- client/src/components/Home/Howitworks.js
+++ client/src/components/Home/Howitworks.js
@@ -2,16 +2,23 @@ import React from "react";
 
 const Howitworks = () => (
   <div className="container-fluid howItWorksContainer">
-    <img className="card-img-top" src={require('../../images/timer.png')} alt="Timer Image" height = "130" />
+    <img
+      className="card-img-top"
+      src={require("../../images/timer.png")}
+      alt="Timer Image"
+      height="130"
+    />
     <h1 className="howItWorksTitle">HOW IT WORKS.</h1>
     <ol>
-      <li className="howItWorksText">1. Create and Accout or Login</li>
+      <li className="howItWorksText">1. Create an Account or Login</li>
       <li className="howItWorksText">
-        2. All you need is the recipe URL. Input the URL into the text area on your
-        homepage. The app will do its magic, and your recipe will be added to
-        your account.
+        2. All you need is the recipe URL. Input the URL into the input area on
+        your homepage. The app will do its magic, and your recipe will be added
+        to your account.
       </li>
-      <li className="howItWorksText">3. Explore our features and start cooking!</li>
+      <li className="howItWorksText">
+        3. Explore our features and start cooking!
+      </li>
     </ol>
   </div>
 );
